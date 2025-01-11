05e6a0ad591db1c2df5c28558fb6a8422d29f231
Sumit Joshi <sumit.joshi0616@gmail.com>
2025-01-12 00:01:54 +0530

    done

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 // start express app
@@ -91,6 +92,8 @@ const limiter = rateLimit({
     message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api',limiter);
+
+app.use('/webhook-checkout' ,express.raw({type:'application/json'}), bookingController.webhookCheckout)
 
 
 //  bodyparer, reading data from body into req.body
