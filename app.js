7ce9efdadcb831ec997ee725b5ec311d09fcd9d9
Sumit Joshi <sumit.joshi0616@gmail.com>
2025-01-11 22:27:22 +0530

    Implemented Cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,8 +27,13 @@ app.set('views' ,path.join(__dirname,'./views'));
 
 // 1) GLOBAL MIDDLEWARE
 // Set security HTTP headers
+// Implement cors
+app.use(cors());
+
+// Access -control-Allow-Origin
 
 
+app.use('*' ,cors());
 
 // serving static file.
 app.use(express.static(path.join(__dirname , 'public')));
